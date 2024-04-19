755f91f77fe9a91ac12b4cbf93042c805ff93e98
ESPIE <remi.espie@etu.umontpellier.fr>
2024-04-19 17:10:13 +0200

    wip: implemented basic functions for mock agent
    Signed-off-by: ESPIE <remi.espie@etu.umontpellier.fr>

--- src/agent/src/agents/mock.rs
+++ src/agent/src/agents/mock.rs
@@ -1,5 +1,7 @@
 use crate::agents::Agent;
 use crate::{workload, AgentResult};
+use std::fs::create_dir_all;
+use std::time::SystemTime;
 
 pub struct MockAgent {
     workload_config: workload::config::Config,
@@ -13,10 +15,33 @@ impl From<workload::config::Config> for MockAgent {
 
 impl Agent for MockAgent {
     fn prepare(&self) -> AgentResult<()> {
-        todo!()
+        let dir = format!("/tmp/{}", self.workload_config.workload_name);
+
+        println!("Function directory: {}", dir);
+
+        create_dir_all(&dir).expect("Unable to create directory");
+
+        std::fs::write(
+            format!("{}/mock.txt", &dir),
+            format!(
+                "Mock agent for {} - written at {:?}",
+                self.workload_config.workload_name,
+                SystemTime::now()
+            ),
+        )
+        .expect("Unable to write main.rs file");
+
+        Ok(())
     }
 
     fn run(&self) -> AgentResult<()> {
-        todo!()
+        let dir = format!("/tmp/{}", self.workload_config.workload_name);
+
+        let content = std::fs::read_to_string(format!("{}/mock.txt", &dir))
+            .expect("Unable to read mock.txt file");
+
+        println!("Mock agent content: {}", content);
+
+        Ok(())
     }
 }
